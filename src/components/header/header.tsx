388df25c33a7a173cf21773abbcfc980447c9f5a
GW <kowonp@gmail.com>
2024-04-05 03:22:43 +0900

    Add Reddit logo to header and display user email

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,23 +1,37 @@
 "use client";
 
 import ROUTES from "@/constants/routes";
+import useUser from "@/hooks/useUser";
 import { firebaseAuth } from "@/libs/firebase";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const Header = () => {
   const router = useRouter();
+  const { user } = useUser();
+
+  const handleClickLogo = () => {
+    router.push(ROUTES.HOME);
+  };
 
   const handleSignOut = async () => {
+    const isOk = confirm("Are you sure you want to sign out?");
+    if (!isOk) {
+      return;
+    }
     await firebaseAuth.signOut();
     router.push(ROUTES.LOGIN);
   };
 
   return (
     <div className="border-gray-300 border-b-2 w-full flex justify-between items-center px-4 py-4">
-      <div>Logo</div>
-      <button type="button" onClick={handleSignOut}>
-        Sign Out
-      </button>
+      <Image onClick={handleClickLogo} src="/icons/ic_reddit.svg" alt="Reddit Logo" width={90} height={90} className="cursor-pointer" />
+      <div className="flex gap-8">
+        <div>{user?.email}</div>
+        <button type="button" onClick={handleSignOut}>
+          Sign Out
+        </button>
+      </div>
     </div>
   );
 };
